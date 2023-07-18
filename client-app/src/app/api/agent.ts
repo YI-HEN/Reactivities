cb598a1963c6d62787df145007f31153991c3af1
YI-HEN <cas830321@gmail.com>
2023-07-18 12:33:51 +0800

    End of section 6

---
+++ client-app/src/app/api/agent.ts
@@ -0,0 +1,44 @@
+import axios, { Axios, AxiosResponse } from "axios";
+import { Activity } from "../models/activity";
+
+const sleep = (delay : number) => {       //建立一個延遲時間，來模擬遠端加載時的數據傳輸狀態
+    return new Promise((resolve) => {
+        setTimeout(resolve , delay)
+    })
+}
+
+axios.defaults.baseURL = 'http://localhost:5000/api';
+
+axios.interceptors.response.use(async response => {
+    try {
+        await sleep(1000);
+        return response;
+    } catch (error) {
+        console.log(error);
+        return await Promise.reject(error);
+    }
+})
+
+const responseBody = <T> (response : AxiosResponse<T>) => response.data;
+
+const requests = {
+    get : <T> (url : string) => axios.get<T>(url).then(responseBody),
+    //post : <T> (url : string , body : {}) => axios.post<T>(url , body).then(responseBody),
+    //put : <T> (url : string , body : {}) => axios.put<T>(url , body).then(responseBody),
+    //del : <T> (url : string) => axios.delete(url).then<T>(responseBody)
+}
+
+const Activities = {
+    list : () => requests.get<Activity[]>('/activities'), 
+    details : (id : string) => requests.get<Activity>(`/activities/${id}`),
+    create : (activity : Activity) => axios.post<void>('/activities' , activity),
+    update : (activity : Activity) => axios.put<void>(`/activities/${activity.id}` , activity),
+    delete : (id : string) => axios.delete<void>(`/activities/${id}`),
+
+}
+
+const agent = { 
+    Activities
+}
+
+export default agent;
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -7,10 +7,11 @@ interface Props{
     activity : Activity | undefined;
     closeForm : () => void;
     createOrEdit : (activity : Activity) => void;
+    submitting : boolean;
 }
 
 
-export default function ActivityForm({activity : selectedActivity , closeForm , createOrEdit} : Props)
+export default function ActivityForm({activity : selectedActivity , closeForm , createOrEdit , submitting} : Props)
 {
     const initialState = selectedActivity ?? {  //若selectedActivity沒有被定義，則初始化selectedActivity，以便給activity繼承
         id : '',
@@ -30,7 +31,7 @@ export default function ActivityForm({activity : selectedActivity , closeForm , 
 
     function handleInputChange(event : ChangeEvent<HTMLInputElement | HTMLTextAreaElement>){
         const {name , value} = event.target;
-        setActivity({...activity , [name] : value})
+        setActivity({...activity , [name] : value});
     }
 
     return(
@@ -39,10 +40,10 @@ export default function ActivityForm({activity : selectedActivity , closeForm , 
                 <Form.Input placeholder='Title' value={activity.title} name='title' onChange={handleInputChange}/>
                 <Form.TextArea placeholder='Description' value={activity.description} name='description' onChange={handleInputChange}/>
                 <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleInputChange}/>
-                <Form.Input placeholder='Date' value={activity.date} name='date' onChange={handleInputChange}/>
+                <Form.Input type='date' placeholder='Date' value={activity.date} name='date' onChange={handleInputChange}/>
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange}/>
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange}/>
-                <Button floated='right' positive type='submit' content='Submit' />
+                <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
                 <Button onClick={closeForm} floated='right' type='button' content='Cancel' />
             </Form>
         </Segment>
