ab92989b5a5e32ffeb93eb395f6647530fa580dc
YI-HEN <cas830321@gmail.com>
2023-07-20 17:27:00 +0800

    End of section 8

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,4 +1,4 @@
-import axios, { Axios, AxiosResponse } from "axios";
+import axios, { AxiosResponse } from "axios";
 import { Activity } from "../models/activity";
 
 const sleep = (delay : number) => {       //建立一個延遲時間，來模擬遠端加載時的數據傳輸狀態
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,25 +1,22 @@
-import React, { useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Outlet, useLocation } from 'react-router-dom';
+import HomePage from '../../features/home/homePage';
 
 function App() {
-
-  const {activityStore} = useStore();
-
-  useEffect(() => { activityStore.loadActivities(); },[activityStore])
-
-  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
+  const location = useLocation();
 
   return (
     <>  {/*  <Fragment>在React的簡寫，return只能返回一個元素，因此要用<Fragment>包裹 */}
-      <NavBar/>
-      <Container style={{marginTop : '7em'}}>
-        <ActivityDashboard />
-      </Container>  
+      {location.pathname === '/' ? <HomePage /> : (
+        <>
+          <NavBar/>
+          <Container style={{marginTop : '7em'}}>
+            <Outlet />
+          </Container>  
+        </>
+      )}
     </>
   );
 }
