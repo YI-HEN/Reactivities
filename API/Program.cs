16cf810fcc279ff28667387d3cb0307fe39a383f
YI-HEN <cas830321@gmail.com>
2023-08-15 17:49:53 +0800

    Security headers added

--- API/Program.cs
+++ API/Program.cs
@@ -28,11 +28,33 @@ var app = builder.Build(); //內建生成
 
 app.UseMiddleware<ExceptionMiddleware>(); //使用中介器<我們做的例外中介器>
 
+app.UseXContentTypeOptions(); //阻止網路嗅探
+app.UseReferrerPolicy(opt => opt.NoReferrer()); //防止DOS/DDOS
+app.UseXXssProtection(opt => opt.EnabledWithBlockMode()); //防止XXS跨網站攻擊
+app.UseXfo(opt => opt.Deny()); //防止在iframe中使用
+app.UseCsp(opt => opt  //防止腳本攻擊(設定白名單)，先用app.UseCspReportOnly增加白名單，增加完再改回app.UseCsp
+    .BlockAllMixedContent() //防止載入http及https混和請求
+    .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com")) //程式的自我樣式設定設為安全
+    .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:")) //程式的自我字體設定設為安全
+    .FormActions(s => s.Self()) //程式的自我動作設為安全
+    .FrameAncestors(s => s.Self()) //自我框架設定安全
+    .ImageSources(s => s.Self().CustomSources("blob:", "https://res.cloudinary.com")) //自我圖片設定安全
+    .ScriptSources(s => s.Self()) //自我腳本設定安全
+); 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) //若在開發模式，則用swagger
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+else
+{
+    app.Use(async (context, next) => //字定義一個標頭(嚴格安全性)
+    {
+        context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+        await next.Invoke();
+    });
 }
 
 app.UseCors("CorsPolicy"); 
